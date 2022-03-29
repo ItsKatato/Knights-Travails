3deba808e9b9d63a685963b1abfec8edcc6a06ad
ItsKatato <kaelahsmallwood@gmail.com>
2022-03-29 11:36:30 -0500

    Working project

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,10 +1,7 @@
 # Frozen_String_Literal: false
 
 class Board
-
-
-  def initialize
-  end
+  def initialize; end
 
   def self.invalid?(array)
     if array[0] < 1 || array[0] > 8
@@ -31,77 +28,93 @@ end
 
 class Node
   include Comparable
-  attr_accessor :data, :left_u, :left_d, :right_u, :right_d, :up_l, :up_r, :down_l, :down_r
+  attr_accessor :data, :previous, :next_spaces, :visited
 
   def initialize(data)
     @data = data
-    @left_u = nil
-    @left_d = nil 
-    @right_u = nil
-    @right_d = nil
-    @up_l = nil
-    @up_r = nil
-    @down_l = nil
-    @down_r = nil
+    @previous = nil
+    @next_spaces = []
+    @visited = false
   end
 end
 
 # Class Knight
 class Knight
-  attr_accessor :root, :all_spaces
+  attr_accessor :all_spaces, :root
 
   def initialize
+    @all_spaces = possible_moves
     @root = nil
-    @all_spaces = nil
-  end
-
-  def any_direction_match?(root)
-    if root.left_u || root.left_d
-      return true
-    elsif root.right_u || root.right_d
-      return true
-    elsif root.down_l || root.down_r
-      return true
-    elsif root.up_l || root.up_r
-      return true
-    else
-      return false
-    end
   end
 
   def find_match(searched_array, array)
-    searched_array.detect { |element| element.data == array}
+    searched_array.detect { |element| element.data == array }
   end
 
-  def possible_moves(start_space)
-
+  def possible_moves
     board_spaces = Board.create_board
 
     board_space_objs = []
-    
-    board_spaces.each { |space| board_space_objs << Node.new(space)}
+
+    board_spaces.each { |space| board_space_objs << Node.new(space) }
 
     board_space_objs.each do |space_obj|
-      space_obj.left_u = find_match(board_space_objs, [(space_obj.data[0] - 2), (space_obj.data[1] + 1)])
-      space_obj.left_d = find_match(board_space_objs, [(space_obj.data[0] - 2), (space_obj.data[1] - 1)])
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] - 2), (space_obj.data[1] + 1)]))
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] - 2), (space_obj.data[1] - 1)]))
 
-      space_obj.right_u = find_match(board_space_objs, [(space_obj.data[0] + 2), (space_obj.data[1] + 1)])
-      space_obj.right_d = find_match(board_space_objs, [(space_obj.data[0] + 2), (space_obj.data[1] - 1)])
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] + 2), (space_obj.data[1] + 1)]))
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] + 2), (space_obj.data[1] - 1)]))
 
-      space_obj.up_l = find_match(board_space_objs, [(space_obj.data[0] - 1), (space_obj.data[1] + 2)])
-      space_obj.up_r = find_match(board_space_objs, [(space_obj.data[0] + 1), (space_obj.data[1] + 2)])
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] - 1), (space_obj.data[1] + 2)]))
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] + 1), (space_obj.data[1] + 2)]))
 
-      space_obj.down_l = find_match(board_space_objs, [(space_obj.data[0] - 1), (space_obj.data[1] - 2)])
-      space_obj.down_r = find_match(board_space_objs, [(space_obj.data[0] + 1), (space_obj.data[1] - 2)])
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] - 1), (space_obj.data[1] - 2)]))
+      space_obj.next_spaces.push(find_match(board_space_objs, [(space_obj.data[0] + 1), (space_obj.data[1] - 2)]))
+
+      space_obj.next_spaces.compact!
     end
     @all_spaces = board_space_objs
-    @root = find_match(board_space_objs, start_space)
+  end
+
+  def knight_moves(start_space, end_space)
+    queue = []
+    start_match = find_match(all_spaces, start_space)
+    start_match.visited = true
+    queue << start_match
+
+    while !queue.empty?
+      current_node = queue.shift
+      current_node.next_spaces.each do |node|
+        if !node.visited
+          node.visited = true
+          queue.push(node)
+          node.previous = current_node
+          if node.data == end_space
+            print_path(end_space)
+            queue.clear
+            break
+          end
+        end
+      end
+    end
+  end
+
+  def print_path(end_space)
+    node = find_match(all_spaces, end_space)
+    route = []
+    while !node.nil?
+      route << node.data
+      node = node.previous
+    end
+    route.reverse!
+    puts "You made it in #{route.length - 1} moves! Here's your path:"
+    p route
   end
 end
 
+
 knight = Knight.new
 
-knight.possible_moves([4,4])
-
-p knight.root
+knight.root = knight.find_match(knight.all_spaces, [4, 4])
+knight.knight_moves([3, 3], [4,3])
 
